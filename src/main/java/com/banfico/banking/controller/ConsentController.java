d1bd7de99a56bc89e976147f06d505b1318092dc
Tilak-Codex <tilak25rj@gmail.com>
2026-09-24 22:52:52 +0530

    feat:add http status created fro post

--- src/main/java/com/banfico/banking/controller/ConsentController.java
+++ src/main/java/com/banfico/banking/controller/ConsentController.java
@@ -2,11 +2,13 @@ package com.banfico.banking.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banfico.banking.dto.ConsentRequest;
@@ -26,6 +28,7 @@ public class ConsentController {
     }
 
     @PostMapping("/customers/{customerId}")
+    @ResponseStatus(HttpStatus.CREATED)
     public ConsentResponse createConsent(
             @PathVariable Long customerId,
             @Valid @RequestBody ConsentRequest request) {
