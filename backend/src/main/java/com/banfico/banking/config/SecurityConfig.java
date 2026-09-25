d7d6934c7221d7296a90dc1721b1ad96228adf17
Tilak-Codex <tilak25rj@gmail.com>
2026-09-25 16:50:56 +0530

    chore(security config): add oauth2 resource server and jwt auth

--- backend/src/main/java/com/banfico/banking/config/SecurityConfig.java
+++ backend/src/main/java/com/banfico/banking/config/SecurityConfig.java
@@ -14,13 +14,16 @@ public class SecurityConfig {
             HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())   //application is using a REST API with JWT bearer authentication rather than traditional server-side session/form authentication. For this API architecture, we'll disable CSRF.
+                .csrf(csrf -> csrf.disable()) // application is using a REST API with JWT bearer authentication rather
+                                              // than traditional server-side session/form authentication. For this API
+                                              // architecture, we'll disable CSRF.
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()                
-                );
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt->{}));   // means now our spring appn is a resource server and requires jwt token to access the endpoints
 
         return http.build();
     }
 }
 
- //  .anyRequest().authenticated()  -> Allow every request (no aunthentication required)
+// .anyRequest().authenticated() -> Allow every request (no aunthentication
+// required)
